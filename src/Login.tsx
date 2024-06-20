69ae25aaa26344db37db72d746b3311db69d5398
Mateus Rodrigues Da Silva <101468435+mateusrws@users.noreply.github.com>
2024-06-20 10:40:34 -0300

    Update Login.tsx

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
-import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
 import { Logar } from './components/Logar';
 import { Header } from './components/Header';
