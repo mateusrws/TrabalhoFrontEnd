1f19769b9d5ba0fa97f3f3110e2c02a65ce3dce3
Mateus Rodrigues Da Silva <101468435+mateusrws@users.noreply.github.com>
2024-06-20 10:41:10 -0300

    Update Logar.ts

--- src/components/Logar.ts
+++ src/components/Logar.ts
@@ -2,10 +2,11 @@ import axios from "axios";
 
 export async function Logar(nome:string, senha:string, navigate: Function) {
     try {
-      const login = await axios.post('http://localhost:3344/login', {
+      const login = await axios.post('https://trabalhobackend.onrender.com/login', {
         cli_password: senha.toLowerCase(),
         cli_nome: nome.toLowerCase()
       });
+      console.log(login)
       localStorage.setItem('cli_nome', nome);
       console.log('Logado com sucesso!');
       navigate('/Home');
